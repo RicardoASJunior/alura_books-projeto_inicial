3d9ec19db513d7c78650b8239a7262fb2b58c4a9
Ricardo <ric.asjunior@gmail.com>
2023-05-01 00:41:26 -0300

    Adicionando ordenação por tag

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,6 +1,7 @@
 const elementoParaInserirLivros = document.getElementById('livros');
 
 function exibirOsLivrosNaTela(listaDeLivros){
+    elementoParaInserirLivros.innerHTML = '';
     listaDeLivros.forEach(livro => {
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
@@ -9,7 +10,7 @@ function exibirOsLivrosNaTela(listaDeLivros){
             ${livro.titulo}
         </h2>
         <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco}</p>
+        <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
         <div class="tags">
           <span class="tag">${livro.categoria}</span>
         </div>
